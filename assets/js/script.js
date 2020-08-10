093d88ef1d78f669818a47543439d8c9edc704f0
condemnJS <gizat.sultan@mail.ru>
2020-08-10 18:06:11 +0700

    parseV1

---
+++ assets/js/script.js
@@ -0,0 +1,142 @@
+class Field {
+    constructor(url) {
+        this.url = url;
+    }
+    async response(){
+        try {
+            let promise = await fetch(`assets/js/json/${this.url}.json`);
+            let response = await promise.json();
+            return response;
+        } catch (e) {
+            console.log('Not Found 404')
+        }
+    }
+}
+class Select {
+    constructor(el, data) {
+        this.$data = data;
+        this.$element = el;
+        this.ArrOptions = ['addpost', 'colorsheme', 'interview', 'signin', 'signup']
+    }
+    FillingSelect(){
+        this.ArrOptions.forEach((item)=>{
+            this.$element.append(`<option>${item}</option>`)
+        });
+    }
+    selectOptions(){
+        this.FillingSelect();
+        this.$element.change((event)=>{
+           new Field(event.target.value).response().then((resolve)=>{
+               this.$data.empty();
+               let data = new DataInfo(resolve, $("#data-info"));
+               data.check();
+               data.reference();
+               data.button();
+           });
+        });
+    }
+
+}
+class DataInfo {
+    constructor(options, el) {
+        this.$el = el;
+        this.option = options;
+        this.name = options.name;
+        this.fields = options.fields;
+        this.references = options.references;
+        this.buttons = options.buttons;
+        this.name = options.name;
+
+        this.required = false;
+        this.multiple = false;
+        this.placeholder = '';
+        this.mask = '';
+        this.checked = false;
+        this.ref = '';
+        this.text = '';
+        this.textWR = '';
+
+        this.maskId = 0;
+    }
+    check() {
+        this.$el.append(`<h1 class="mb-3">${this.name}</h1>`)
+        this.fields.map((name)=>{
+            this.required = name.input.required || false;
+            this.multiple = name.input.multiple || false;
+            this.placeholder = name.input.placeholder || '';
+            this.mask = name.input.mask || '';
+            if(name.input.type === 'textarea'){
+                this.$el.append(`<div class="form-group">
+                                    <label for="inp-control">${name.label}</label>
+                                    <textarea required="${this.required}" id="inp-control" class="form-control" rows="5"></textarea>
+                                </div>`)
+            }else if(!name.label){
+                this.$el.append(`<div class="form-group">
+                                    <input type="${name.input.type}" required="${this.required}" multiple="${this.multiple}" placeholder="${this.placeholder}" id="inp-control" class="form-control">
+                                </div>`)
+            }else if(name.input.type === 'color'){
+                this.$el.append(`<div class="form-group" id="colors">
+                                    <input value="#3366ff" type="${name.input.type}" required="${this.required}" multiple="${this.multiple}" placeholder="${this.placeholder}" id="inp-control" class="form-control" list="colorList">
+                                </div>`)
+                $('#colors').append('<datalist id="colorList"></datalist>')
+                name.input.colors.forEach((item)=>{
+                    $('#colorList').append(`<option value="${item}" label="${item}">`)
+                });
+            }else if(name.input.type === 'technology'){
+                this.$el.append(`<div class="form-group">
+                                   <select id="${name.input.type}" class="form-control" type="${name.input.type}" required="${this.required}" multiple="${this.multiple}"></select>
+                                </div>`)
+                name.input.technologies.forEach((item)=>{
+                    $(`#${name.input.type}`).append(`<option value="${item}" label="${item}">`)
+                });
+            }else if(name.input.mask){
+                this.$el.append(`<div class="form-group">
+                                   <label class="w-100">${name.label}
+                                       <input type="text" required="${this.required}" multiple="${this.multiple}"  placeholder="${this.placeholder}" class="form-control mask mt-2" id="mask_${this.maskId}">
+                                   </label>
+                                </div>`);
+                $(`#mask_${this.maskId}`).mask(name.input.mask);
+
+                this.maskId++;
+            }else {
+                this.$el.append(`<div class="form-group">
+                                    <label for="inp-control">${name.label}</label>
+                                    <input type="${name.input.type}" required="${this.required}" multiple="${this.multiple}"  placeholder="${this.placeholder}" id="inp-control" class="form-control">
+                                </div>`)
+            }
+        })
+    }
+    reference(){
+        this.references.forEach((item)=>{
+            let log = Object.keys(item);
+            if(log[0] === 'input'){
+                for(let i in item){
+                    this.required = item[i].required;
+                    this.checked = item[i].checked;
+                    this.$el.append(`<div class="form-group" style="display:inline">
+                                        <${i} type="${item[i].type}" required="${this.required}" checked="${this.checked}" id="checked">
+                                    </div>`)
+                    if(this.checked != 'true') {
+                        $('#checked').attr('checked', false)
+                    }
+                }
+            }else {
+                this.ref = item.ref || '';
+                this.text = item.text || '';
+                this.textWR = item['text without ref'] || '';
+                this.$el.append(`<p style="display:inline;">
+                                    ${this.textWR}
+                                    <a href="${this.ref}">${this.text}</a>
+                                </p>`)
+            }
+        });
+    }
+    button(){
+        this.$el.append(`<div class="form-group buttons"></div>`);
+        this.buttons.forEach((item)=>{
+            $(`.buttons`).append(`<button class="btn btn-primary mt-3 mr-3">${item.text}</button>`)
+        });
+    }
+}
+let output = new Select($("#select-control"), $("#data-info"));
+output.selectOptions();
